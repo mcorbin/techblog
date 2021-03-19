31322685cd3025ee5d9ca18a917aa44642dae90c
mcorbin <corbin.math@gmail.com>
2021-03-19 21:20:20 +0100

    release v0.0.2

--- release.sh
+++ release.sh
@@ -1,7 +1,10 @@
 set -e
 tag=$1
 lein run
+git add .
+git commit -m "release ${tag}"
 git tag -a "${tag}" -m "release ${tag}"
 docker build -t mcorbin/techblog:${tag} .
 docker push mcorbin/techblog:${tag}
 git push --tags
+git push
